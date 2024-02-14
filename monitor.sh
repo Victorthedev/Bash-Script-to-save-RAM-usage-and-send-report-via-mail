227c94e154f9d2956eb125ccb98d694f2f4ec38a
Victor <ubahakweemeka@gmail.com>
2024-02-14 10:48:26 +0000

    added script

---
+++ monitor.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+memory_file="/memo.txt"
+email_address="ubahakweemeka@gmail.com"
+
+usage() {
+	current_time=$(date +"%Y-%m-%d %H:%M:%S")
+	memory_usage=$(free -m | awk 'NR == 2 { printf "%.2f%%", $3*100/2 }')
+	echo "$current_time $memory_usage" >> "$memory_file"
+}
+
+send_report() {
+	subject="Memory Usage - $(date +'%Y-%m-%d')"
+	body=$(cat "$memory_file")
+	echo "$body" | mail -s "$subject" "$email_address"
+	rm "$memory_file"
+}
+
+midnight() {
+	current_hour=$(date +"%H")
+	if [ "$current_hour" == "00" ]; then
+	send_report
+	fi
+}
+
+while true; do
+	usage
+	midnight
+	sleep 3600
+done
+
+
+
+
+
+
+
+
+
